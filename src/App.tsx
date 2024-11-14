5a939f3840de6379fde3b097e4dc5abaa68a507c
Darien Sim <you@example.com>
2024-11-14 17:05:39 +0000

    trip page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './HomePage';
+import TripPage from './TripPage';
 import darkTheme from './theme';
-
 
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-      <HomePage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<TripPage />} />
+          <Route path="/:id" element={<HomePage />} />
+        </Routes>
+      </Router>
     </ThemeProvider>
   );
 }
