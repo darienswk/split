752b64c0da73221bd53b86f11cec56c2218fc922
Darien <darienswk@gmail.com>
2024-11-18 14:16:38 +0800

    add charts

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,11 +1,31 @@
 import axios from 'axios';
-import { Grid, Typography, Container, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, TextField, FormControlLabel, FormLabel, Radio, RadioGroup, Button, Card, CardHeader, IconButton, Chip, Avatar, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
-import React, { useEffect, useState } from "react";
+import { Grid, Typography, Container, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, TextField, FormControlLabel, FormLabel, Radio, RadioGroup, Button, Card, CardHeader, IconButton, Chip, Avatar, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, ListItemIcon, ListItemText, Box } from "@mui/material";
+import React, { useCallback, useEffect, useState, useMemo } from "react";
 import DeleteIcon from '@mui/icons-material/Delete';
+import RestaurantIcon from '@mui/icons-material/Restaurant';
+import AttractionsIcon from '@mui/icons-material/Attractions';
+import ReceiptIcon from '@mui/icons-material/Receipt';
+import FlightIcon from '@mui/icons-material/Flight';
+import LocalTaxiIcon from '@mui/icons-material/LocalTaxi';
+import HotelIcon from '@mui/icons-material/Hotel';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+
 import Dudu from "./dudu.jpg";
 import Bubu from "./bubu.jpg";
+import PieChart from './PieChart';
+
+const categoryToIconMap = {
+    "General": <ReceiptIcon />,
+    "Dining": <RestaurantIcon />,
+    "Transport": <LocalTaxiIcon />,
+    "Shopping": <ShoppingBagIcon />,
+    "Attractions": <AttractionsIcon />,
+    "Accommodation": <HotelIcon />,
+    "Flights": <FlightIcon />,
+};
 
 const API_URL = "https://split-server-t63h.onrender.com";
+// const API_URL = "http://localhost:5000";
 
 const convertTimestampToDate = (timestamp: { seconds: number, nanoseconds: number }): Date => {
     return new Date(timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000);
@@ -27,11 +47,15 @@ interface Item {
     amount: number;
     description: string;
     payment: string;
+    category: keyof typeof categoryToIconMap;
 }
 
 interface Summary {
     spent: number;
     owed: number;
+    categorySpent: {
+        [category: string]: number;
+    }
 }
 
 
@@ -44,10 +68,30 @@ const HomePage: React.FC = () => {
     const [amount, setAmount] = useState<number | null>(null);
     const [description, setDescription] = useState("");
     const [payment, setPayment] = useState(RADIO_OPTIONS[0]);
+    const [category, setCategory] = useState("General");
     const [summary, setSummary] = useState<{ [key: string]: Summary }>({
-        DS: { spent: 0, owed: 0 },
-        KT: { spent: 0, owed: 0 },
+        DS: { spent: 0, owed: 0, categorySpent: {} },
+        KT: { spent: 0, owed: 0, categorySpent: {} },
     });
+    const [conversionRates, setConversionRates] = useState<{ [currency: string]: number } | null>(null);
+
+    // For filtering
+    const [selectedChart, setSelectedChart] = useState<string | null>(null);
+    const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+    const [selectedPiece, setSelectedPiece] = useState<{ chart: string; category: string } | null>(null);
+
+    const handleSetSelectedChart = useCallback((chart: string) => {
+        setSelectedChart(chart);
+    }, []);
+
+    const handleSetSelectedCategory = useCallback((category: string) => {
+        setSelectedCategory(category);
+    }, []);
+
+    const handleSetSelectedPiece = useCallback((piece: { chart: string; category: string } | null) => {
+        setSelectedPiece(piece);
+    }, []);
+
 
     const handleClickOpen = (itemId: string) => {
         setDialogOpen(true);
@@ -68,6 +112,10 @@ const HomePage: React.FC = () => {
         setCurreny(event.target.value as string);
     }
 
+    const handleChange = (event: SelectChangeEvent) => {
+        setCategory(event.target.value as string);
+    };
+
     const onAmountChange = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>): void => {
         setAmount(Number(event.target.value));
     };
@@ -82,7 +130,7 @@ const HomePage: React.FC = () => {
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
         // Add form submission logic here
-        const newItem = { paidBy, currency, amount: amount ?? 0, description, payment };
+        const newItem = { paidBy, currency, amount: amount ?? 0, description, payment, category };
 
         if (amount !== null && amount < 0) {
             return;
@@ -98,29 +146,38 @@ const HomePage: React.FC = () => {
     };
 
     useEffect(() => {
+        // TODO: update for all currencies
+        const MYR_EXCHANGE_RATE = conversionRates?.["MYR"] ?? 3.38;
         const calculateSummary = () => {
             const summary: { [key: string]: Summary } = {
-                DS: { spent: 0, owed: 0 },
-                KT: { spent: 0, owed: 0 },
+                DS: { spent: 0, owed: 0, categorySpent: {} },
+                KT: { spent: 0, owed: 0, categorySpent: {} },
             };
 
             data.forEach(item => {
-                const { paidBy, amount, payment, currency } = item;
-                const cost = currency === "SGD" ? amount : amount / 3.45;
+                const { paidBy, amount, payment, currency, category } = item;
+                const cost = currency === "SGD" ? amount : amount / MYR_EXCHANGE_RATE;
 
                 if (payment === 'Tracking') {
                     summary[paidBy].spent += cost;
+                    summary[paidBy].categorySpent[category] = (summary[paidBy].categorySpent[category] || 0) + cost;
                 } else if (payment === 'Split equally') {
-                    // if split both spent same amount
-                    summary["DS"].spent += cost / 2;
-                    summary["KT"].spent += cost / 2;
+                    const halfCost = cost / 2;
+
+                    summary["DS"].spent += halfCost;
+                    summary["DS"].categorySpent[category] = (summary["DS"].categorySpent[category] || 0) + halfCost;
+
+                    summary["KT"].spent += halfCost;
+                    summary["KT"].categorySpent[category] = (summary["KT"].categorySpent[category] || 0) + halfCost;
+
                     const otherUser = paidBy === 'DS' ? 'KT' : 'DS';
-                    summary[otherUser].owed += cost / 2;
+                    summary[otherUser].owed += halfCost;
                 } else if (payment === 'Owed full amount') {
-                    // other user owes and spends the cost
                     const otherUser = paidBy === 'DS' ? 'KT' : 'DS';
+
                     summary[otherUser].owed += cost;
                     summary[otherUser].spent += cost;
+                    summary[otherUser].categorySpent[category] = (summary[otherUser].categorySpent[category] || 0) + cost;
                 }
             });
 
@@ -128,7 +185,8 @@ const HomePage: React.FC = () => {
         };
 
         calculateSummary();
-    }, [data]);
+    }, [data, conversionRates]);
+
 
     useEffect(() => {
         const fetchData = async () => {
@@ -140,7 +198,17 @@ const HomePage: React.FC = () => {
             }
         };
 
+        const fetchConversionRates = async () => {
+            try {
+                const response = await axios.get(`${API_URL}/exchange-rates`)
+                setConversionRates(response.data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
         fetchData();
+        // fetchConversionRates();
     }, []);
 
     const handleDelete = async () => {
@@ -155,10 +223,19 @@ const HomePage: React.FC = () => {
         handleClose();
     };
 
+    const pieChart = useMemo(() => <PieChart
+        summary={summary}
+        selectedChart={selectedChart}
+        setSelectedChart={handleSetSelectedChart}
+        selectedCategory={selectedCategory}
+        setSelectedCategory={handleSetSelectedCategory}
+        selectedPiece={selectedPiece}
+        setSelectedPiece={handleSetSelectedPiece}
+    />, [handleSetSelectedCategory, handleSetSelectedChart, handleSetSelectedPiece, selectedCategory, selectedChart, selectedPiece, summary]);
+
     if (data.length === 0) {
         return <p>Loading...</p>;
     }
-
 
     return <Container>
         <Grid container spacing={3}>
@@ -166,7 +243,7 @@ const HomePage: React.FC = () => {
                 <Typography>This app is in beta</Typography>
             </Grid>
             <form onSubmit={handleSubmit}>
-                <Grid item xs={12} container spacing={2} mt={2} pl={2}>
+                <Grid container spacing={2} mt={2} pl={2}>
                     <Grid item xs={12}>
                         <FormControl fullWidth>
                             <InputLabel id="paidBy">Paid by</InputLabel>
@@ -226,6 +303,33 @@ const HomePage: React.FC = () => {
                         </FormControl>
                     </Grid>
                     <Grid item xs={12} >
+                        <FormControl variant="outlined" fullWidth>
+                            <InputLabel id="category-select-label">Category</InputLabel>
+                            <Select
+                                labelId="category-select-label"
+                                id="category-select"
+                                value={category}
+                                onChange={handleChange}
+                                label="Category"
+                                renderValue={(selected) => (
+                                    <Box display="flex" alignItems="center">
+                                        {categoryToIconMap[selected as keyof typeof categoryToIconMap]}
+                                        <span style={{ marginLeft: 8 }}>{selected as string}</span>
+                                    </Box>
+                                )}
+                            >
+                                {Object.entries(categoryToIconMap).map(([key, Icon]) => (
+                                    <MenuItem key={key} value={key}>
+                                        <ListItemIcon>
+                                            {Icon}
+                                        </ListItemIcon>
+                                        <ListItemText primary={key} />
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    </Grid>
+                    <Grid item xs={12} >
                         <FormControl fullWidth>
                             <TextField
                                 value={description}
@@ -243,15 +347,21 @@ const HomePage: React.FC = () => {
                 </Grid>
             </form>
             <br />
+            <Grid item container xs={12} spacing={1}>
+                <Grid item xs={12}>
+                    <Typography variant="h6">Summary</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    {pieChart}
+                </Grid>
+                {/* <Grid item xs={12}>
+                    {summary.DS.owed > summary.KT.owed ? <Typography>DS owes KT: {(summary.DS.owed - summary.KT.owed).toFixed(2)} SGD</Typography> : <Typography>KT owes DS: {(summary.KT.owed - summary.DS.owed).toFixed(2)} SGD</Typography>}
+                </Grid> */}
+            </Grid>
             <Grid item xs={12}>
-                <Typography variant="h6">Summary</Typography>
-                <Typography>DS spent: {summary.DS.spent.toFixed(2)} SGD</Typography>
-                <Typography>DS owes: {summary.DS.owed.toFixed(2)} SGD</Typography>
-                <Typography>KT spent: {summary.KT.spent.toFixed(2)} SGD</Typography>
-                <Typography>KT owes: {summary.KT.owed.toFixed(2)} SGD</Typography>
-                <br />
-                {summary.DS.owed > summary.KT.owed ? <Typography>DS owes KT: {(summary.DS.owed - summary.KT.owed).toFixed(2)} SGD</Typography> : <Typography>KT owes DS: {(summary.KT.owed - summary.DS.owed).toFixed(2)} SGD</Typography>
-                }
+                <Typography>
+                    Selected Chart: {selectedChart}, Selected Category: {selectedCategory}
+                </Typography>
             </Grid>
             <Grid item xs={12}
                 sx={{
@@ -264,6 +374,11 @@ const HomePage: React.FC = () => {
                 {data.map((item, index) => (
                     <Card key={index} sx={{ mt: 1, mb: 2, width: "100%" }}>
                         <CardHeader
+                            avatar={
+                                <Avatar>
+                                    {categoryToIconMap[item.category as keyof typeof categoryToIconMap]}
+                                </Avatar>
+                            }
                             title={`${item.description}: ${item.currency} ${item.amount}`}
                             subheader={`[${item.payment}] Paid by: ${item.paidBy} on ${convertTimestampToDate(item.createdAt).toLocaleString("en-GB")}`}
                             action={
@@ -280,6 +395,7 @@ const HomePage: React.FC = () => {
                 ))}
             </Grid>
         </Grid>
+        {/* Deletion Dialog */}
         <Dialog
             open={dialogOpen}
             onClose={handleClose}
